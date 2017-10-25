c450076f389547f07f96487a3d748177de173c2a
Jason <921891608@qq.com>
2017-10-25 19:47:47 +0800

    Add GravityDo Sensor

--- Firmware/WaterMonitor/WaterMonitor/GravitySensorHub.cpp
+++ Firmware/WaterMonitor/WaterMonitor/GravitySensorHub.cpp
@@ -21,7 +21,8 @@
 #include "GravityOrp.h"
 #include "GravityEc.h"
 #include "GravityTemperature.h"
-#include "SensorDo.h"
+//#include "SensorDo.h"
+#include "GravityDo.h"
 #include "SdService.h"
 
 //********************************************************************************************
@@ -43,7 +44,7 @@ GravitySensorHub::GravitySensorHub()
 
 	this->sensors[0] = new GravityPh();
 	this->sensors[1] = new GravityTemperature(5);
-	this->sensors[2] = new SensorDo();
+	this->sensors[2] = new GravityDo();
 	this->sensors[3] = new GravityEc(this->sensors[1]);
 	this->sensors[4] = new GravityOrp();
 
