4e1711e501b10ca35d8f0f910245bf75879b8352
Frankie Juncheng <frankdfrayoud34@163.com>
2017-10-30 21:36:51 +0800

    WaterMonitor.ino

--- Software_v1.0/WaterMonitor/WaterMonitor/WaterMonitor.ino
+++ Software_v1.0/WaterMonitor/WaterMonitor/WaterMonitor.ino
@@ -49,20 +49,42 @@
 #include "SdService.h"
 #include "Debug.h"
 
-#include <LiquidCrystal.h>										//add a new library
+//############################################################################################
+
+#include <LiquidCrystal.h>					 //COMMENT*** import a new library here
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);         //COMMENT*** select the pins used on the LCD panel
+											 //COMMENT*** define some values used by the panel and buttons
+int lcd_key     = 0;
+int adc_key_in  = 0;
+
+#define btnRIGHT  0
+#define btnUP     1
+#define btnDOWN   2
+#define btnLEFT   3
+#define btnSELECT 4
+#define btnNONE   5
+//############################################################################################
 
 // clock module
 GravityRtc rtc;
 
 // sensor monitor
 GravitySensorHub sensorHub;
-SdService sdService = SdService(sensorHub.sensors);
+SdService sdService = SdService(sensorHub.sensors);              
 
 void setup() {
 	Serial.begin(9600);
 	rtc.setup();
 	sensorHub.setup();
 	sdService.setup();
+
+//############################################################################################
+//* ***************************** LCD screen initialize setting ************** ************ * /
+
+// 	lcd.begin(16, 2);              //COMMENT*** Start the screen
+// 	lcd.setCursor(0,0);			   //COMMENT*** Initial the cursor 
+// 	lcd.print("Hello, World");	   //COMMENT*** Print 'Hello, World' on the screen
+//############################################################################################
 
 }
 
@@ -85,6 +107,7 @@ void loop() {
 	sensorHub.update();
 	sdService.update();
 
+
 		// ************************* Serial debugging ******************
 //	if(millis() - updateTime > 1000)
 //  {
@@ -101,6 +124,45 @@ void loop() {
 //    Serial.println(sensorHub.getValueBySensorNumber(4));
 //  }
 
+//############################################################################################
+		//* ***************************** LCD screen & Buttons debugging **************
+
+/*
+lcd.setCursor(9,1);             // move cursor to second line "1" and 9 spaces over
+   lcd.print(millis()/1000);       // display seconds elapsed since power-up
+
+   lcd.setCursor(0,1);             // move to the begining of the second line
+   lcd_key = read_LCD_buttons();   // read the buttons
+
+   switch (lcd_key){               // depending on which button was pushed, we perform an action
+
+       case btnRIGHT:{             //  push button "RIGHT" and show the word on the screen
+            lcd.print("RIGHT ");
+            break;
+       }
+       case btnLEFT:{
+             lcd.print("LEFT   "); //  push button "LEFT" and show the word on the screen
+             break;
+       }    
+       case btnUP:{
+             lcd.print("UP    ");  //  push button "UP" and show the word on the screen
+             break;
+       }
+       case btnDOWN:{
+             lcd.print("DOWN  ");  //  push button "DOWN" and show the word on the screen
+             break;
+       }
+       case btnSELECT:{
+             lcd.print("SELECT");  //  push button "SELECT" and show the word on the screen
+             break;
+       }
+       case btnNONE:{
+             lcd.print("NONE  ");  //  No action  will show "None" on the screen
+             break;
+       }
+   }
+*/
+//############################################################################################	
 
 }
 
@@ -135,4 +197,55 @@ void loop() {
 //Serial.print(rtc.minute);
 //Serial.print("   Second = ");//second
 //Serial.println(rtc.second);
+
+
+//############################################################################################
+//* ***************************** LCD screen & Buttons debugging (Function)************** ************ * /
+/*
+int read_LCD_buttons(){               // read the buttons
+    adc_key_in = analogRead(0);       // read the value from the sensor 
+
+    // my buttons when read are centered at these valies: 0, 144, 329, 504, 741
+    // we add approx 50 to those values and check to see if we are close
+    // We make this the 1st option for speed reasons since it will be the most likely result
+
+    if (adc_key_in > 1000) return btnNONE; 
+
+    // For V1.1 us this threshold
+    if (adc_key_in < 50)   return btnRIGHT;  
+    if (adc_key_in < 250)  return btnUP; 
+    if (adc_key_in < 450)  return btnDOWN; 
+    if (adc_key_in < 650)  return btnLEFT; 
+    if (adc_key_in < 850)  return btnSELECT;  
+
+   // For V1.0 comment the other threshold and use the one below:
+   /*
+     if (adc_key_in < 50)   return btnRIGHT;  
+     if (adc_key_in < 195)  return btnUP; 
+     if (adc_key_in < 380)  return btnDOWN; 
+     if (adc_key_in < 555)  return btnLEFT; 
+     if (adc_key_in < 790)  return btnSELECT;   
+   */
+
+    return btnNONE;                // when all others fail, return this.
+}
+*/
+//############################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
